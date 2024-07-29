b0ded7598c31203b59e7e08589df37857f756690
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2024-07-30 08:52:14 +0900

    スクリプト修正

--- Assets/Script/System/Stage/StageManager.cs
+++ Assets/Script/System/Stage/StageManager.cs
@@ -6,21 +6,37 @@ public class StageManager : MonoBehaviour
 {
     public int stageCount = 0;
 
+    public bool counter = false;
+    public bool counted = false;
+
     // Update はフレームごとに呼ばれる
     public void Update()
     {
         // ゲーム内の Goal タグが付いたオブジェクトがすべて消えたかチェック
         if (GameObject.FindGameObjectsWithTag("Goal").Length == 0)
         {
-            // ステージカウンターを増やす
+            counter = true;
             StageCounter();
+        }
+
+        if (GameObject.FindGameObjectsWithTag("Goal").Length == 1)
+        {
+            counter = false;
+            counted = false;
         }
     }
 
     // ステージカウンターを増やす関数
     public void StageCounter()
     {
-        stageCount++;
-        Debug.Log("ステージカウント: " + stageCount);
+        if (counter)
+        {
+            if (!counted)
+            {
+                stageCount++;
+                Debug.Log("ステージカウント: " + stageCount);
+                counted = true;
+            }
+        }
     }
 }
